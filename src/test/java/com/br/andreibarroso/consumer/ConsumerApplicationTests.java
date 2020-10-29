7b10a305c64562f47a0236f226cae24625e4bc77
andreibarroso <andrei.mesquita@globomail.com>
2020-10-29 14:57:55 -0300

    firt commit

---
+++ src/test/java/com/br/andreibarroso/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.andreibarroso.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
